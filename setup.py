6d898d2f0d50e49c619bdf20c901d0cbc2ab0709
danielmbaluka <danielmbaluka@gmail.com>
2020-10-23 17:41:00 +0300

    Revert "remove index migration"
    This reverts commit a106fae2196062c28c21bd0255560da4b3b183fc.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-waffle-session',
-    version='1.0.3',
+    version='1.0.2',
     description='A feature flipper for Django.',
     long_description=open('README.rst').read(),
     author='Francis Mwangi',
